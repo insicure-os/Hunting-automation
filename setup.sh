658246a05dbca235a5cffef6fcb822bd531436b2
LL_RedTeam_GZaccaria <giovanni.zaccaria@lantechlongwave.it>
2024-08-02 16:45:13 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,17 +4,17 @@ echo "Starting setup process..."
 
 # Step 1: Install required tools
 echo "Installing required tools..."
-bash dependencies/install-required-tools.sh
+bash $HOME/Hunting-automation/dependencies/install-required-tools.sh
 echo "Required tools installed successfully!"
 
 # Step 2: Install Python dependencies
 echo "Installing Python dependencies..."
-python3 -m pip install -r dependencies/requirements.txt
+python3 -m pip install -r $HOME/Hunting-automation/dependencies/requirements.txt
 echo "Python dependencies installed successfully!"
 
 # Step 3: Download Nuclei templates
 echo "Downloading Nuclei templates..."
-python3 dependencies/nuclei-templates-downloader.py
+python3 $HOME/Hunting-automation/dependencies/nuclei-templates-downloader.py
 echo "Nuclei templates downloaded successfully!"
 
 echo "Setup process complete!"
